6eaa4708e4a7d6ed83e3cd69d8bb5f291c5b2f4b
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2023-11-17 16:55:58 +0100

    Adding Node type

--- bst.go
+++ bst.go
@@ -1,5 +1,12 @@
 package bst
 
+type Node struct {
+	key         int
+	val         string
+	left, right *Node
+}
+
 func insert() bool {
+	// TODO: Implement
 	return true
 }
--- bst_test.go
+++ bst_test.go
@@ -10,3 +10,11 @@ func TestInsert(t *testing.T) {
 		t.Error("Insert returned false")
 	}
 }
+
+func TestNodeCreation(t *testing.T) {
+	newNode := Node{key: 1, val: "data", left: nil, right: nil}
+
+	if newNode.key != 1 || newNode.val != "data" {
+		t.Errorf("Expected key: 1, val: data. Received key: %v, val: %v", newNode.key, newNode.val)
+	}
+}
